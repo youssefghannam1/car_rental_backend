4da534a6d54ab3c6b210e4a11a4f27747e1c914a
youssefghannam1 <mryoussefghannam@gmail.com>
2024-12-20 21:46:51 +0100

    ok

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,12 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +25,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        DB::prohibitDestructiveCommands(App::isProduction());
+        Model::shouldBeStrict();
+        Model::unguard();
+        URL::forceScheme('https');
+        Vite::usePrefetchStrategy('aggressive');
+        Schema::defaultStringLength(191);
     }
 }
